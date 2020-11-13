457fb79e3e8bf428a10d7fa9de169f6ac7ede916
joshirelle <34638458+joshirelle@users.noreply.github.com>
2020-11-13 19:39:45 +0800

    y

--- adventure.php
+++ adventure.php
@@ -11,7 +11,7 @@
     <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
     <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
     <link href="css/main.css" type="text/css" rel="stylesheet" media="screen,projection" />
-    <link href="css/calendar.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <!-- <link href="css/calendar.css" type="text/css" rel="stylesheet" media="screen,projection" /> -->
 
 </head>
 
@@ -27,7 +27,25 @@
             </ul>
 
             <ul id="nav-mobile" class="sidenav">
-                <li><a href="#">Navbar Link</a></li>
+                <li>
+                    <div class="user-view">
+                        <div class="background">
+                            <img src="./assets/images/raw/image5.jpg" />
+                        </div>
+                        <a href="#user"><img class="circle" src="./assets/images/raw/image5.jpg" /></a>
+                        <a href="#name"><span class="white-text name">Aron Magic Island</span></a>
+                        <a href="#email"><span class="white-text email">aronmagicislandpicnic2@gmail.com</span></a>
+                    </div>
+                </li>
+                <li>
+                    <a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a>
+                </li>
+                <li><a href="#!">Second Link</a></li>
+                <li>
+                    <div class="divider"></div>
+                </li>
+                <li><a class="subheader">Subheader</a></li>
+                <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
             </ul>
             <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         </div>
@@ -79,9 +97,9 @@
                     <li>
                         <div class="collapsible-header"><i class="material-icons">date_range</i>Select date and options</div>
                         <div class="collapsible-body">
-                            <!-- Check in: <input type="text" class="datepicker">
-                            Check out: <input type="text" class="datepicker"> -->
-                            <div class="app-calendar"></div>
+                            Check in: <input type="text" class="datepicker">
+                            Check out: <input type="text" class="datepicker">
+                            <!-- <div class="app-calendar"></div> -->
                         </div>
                     </li>
                 </ul>
@@ -140,7 +158,7 @@
     <!--  Scripts-->
     <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="js/materialize.js"></script>
-    <script src="js/calendar.js" type="module"></script>
+    <!-- <script src="js/calendar.js" type="module"></script> -->
     <script src="js/main.js"></script>
 
 </body>
